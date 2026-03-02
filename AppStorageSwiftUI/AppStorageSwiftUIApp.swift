6c0e86720ad5bd290ce8a7cfc400630bffef73d6
sakurakotubaki <gitonly543@gmail.com>
2026-03-02 10:17:03 +0900

    Initial Commit

---
+++ AppStorageSwiftUI/AppStorageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageSwiftUIApp.swift
+//  AppStorageSwiftUI
+//
+//  Created by 橋本純一 on 2026/03/02.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStorageSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppStorageSwiftUI
+//
+//  Created by 橋本純一 on 2026/03/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
